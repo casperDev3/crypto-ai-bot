1c2349d0915ae6ee205879709caedef592fa4b24
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2024-04-26 13:17:58 +0300

    add changes

--- creds/main.py
+++ creds/main.py
@@ -1,13 +1,21 @@
+import psycopg2
 from aiogram import Dispatcher, Bot
 
 TOKEN = "7122875435:AAHDHh1PjOUWyBXuWiXI0SqanAg8ajRs3Ck"
 
 # Database connection settings
-DB_HOST = "localhost"
-DB_NAME = "mydatabase"
-DB_USER = "yourusername"
-DB_PASS = "yourpassword"
+DB_HOST = "db-postgresql-nyc3-crypto-bot-ai-do-user-14569518-0.c.db.ondigitalocean.com"
+DB_NAME = "defaultdb"
+DB_USER = "doadmin"
+DB_PASS = "AVNS_5IWnjO3mhBd_dSST_Af"
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 bot = Bot(TOKEN)
+
+# Connection to database
+print("Connecting to database...")
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port="25060")
+print(conn)
+cursor = conn.cursor()
+print("Connected to database!")
--- db/main.py
+++ db/main.py
@@ -1,37 +1,31 @@
 from creds import main as c
 import json
 
+cursor = c.cursor
+
 
 def create_new_user(user_data):
-    with open("db/users.json", "r") as file:
-        users = json.load(file)
+    cursor.execute("SELECT * FROM users WHERE chat_id = %s", (user_data["chat_id"],))
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO users (chat_id, first_name, last_name, locale, username) VALUES (%s, %s, %s, %s, "
+                       "%s)",
+                       (user_data["chat_id"], user_data["first_name"], user_data["last_name"], user_data["locale"],
+                        user_data["username"]))
 
-    for user in users:
-        if user["chat_id"] == user_data["chat_id"]:
-            return
-
-    users.append(user_data)
-
-    with open("db/users.json", "w") as f:
-        json.dump(users, f)
+    c.conn.commit()
 
 
 def update_locale_user(chat_id, locale):
-    with open("db/users.json", "r") as file:
-        users = json.load(file)
-
-    for u in users:
-        if u["chat_id"] == chat_id:
-            u["locale"] = locale
-
-    with open("db/users.json", "w") as f:
-        json.dump(users, f)
+    cursor.execute("UPDATE users SET locale = %s WHERE chat_id = %s", (locale, chat_id))
+    c.conn.commit()
 
 
 def get_current_lang(chat_id):
-    with open("db/users.json", "r") as file:
-        users = json.load(file)
-
-    for u in users:
-        if u["chat_id"] == chat_id:
-            return u["locale"]
+    cursor.execute("SELECT locale FROM users WHERE chat_id = %s", (chat_id,))
+    result = cursor.fetchone()
+    if result is not None:
+        print("Answer from db", result[0])
+        return result[0]
+    else:
+        print("No data found for chat_id", chat_id)
+        return None
